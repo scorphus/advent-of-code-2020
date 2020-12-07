9bc74f1605150933de956e1eee3138ac587d4dfc
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2020-12-07 07:12:22 +0100

    Add solution to Day 7

---
+++ aoc/day07.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of Advent of Code 2020
+# https://github.com/scorphus/advent-of-code-2020
+
+# Licensed under the BSD-3-Clause license:
+# https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2020, Pablo S. Blum de Aguiar <scorphus@gmail.com>
+
+import collections
+import graphlib
+
+
+def parse_bags(bags):
+    bag_list = []
+    for bag in bags.split(", "):
+        qtt, shade, color, _ = bag.split(maxsplit=3)
+        bag_list.append((int(qtt), f"{shade} {color}"))
+    return bag_list
+
+
+def parse_line(line):
+    if " contain no " not in line:
+        bag, bags = line.strip().split(" bags contain ")
+    else:
+        bag, _ = line.strip().split(" bags contain ")
+        bags = None
+    return bag, parse_bags(bags) if bags else []
+
+
+def bfs(bags, start):
+    visited = {start}
+    next_bags = collections.deque([start])
+    while next_bags:
+        bag = next_bags.popleft()
+        if bag not in bags:
+            continue
+        for next_bag in bags[bag]:
+            if next_bag not in visited:
+                next_bags.append(next_bag)
+                visited.add(next_bag)
+    return visited
+
+
+def part1(lines):
+    graph = {}
+    for line in lines:
+        bag, bags = parse_line(line)
+        for _, each_bag in bags:
+            graph.setdefault(each_bag, []).append(bag)
+    visited = bfs(graph, "shiny gold")
+    return len(visited) - 1
+
+
+def part2(lines):
+    graph = {}
+    bags_in_bag = {}
+    for line in lines:
+        bag, bags = parse_line(line)
+        graph[bag] = set(b for _, b in bags)
+        bags_in_bag[bag] = bags
+    qtt_acc = {}
+    for bag in graphlib.TopologicalSorter(graph).static_order():
+        total = 1
+        for qtt, bag_in in bags_in_bag[bag]:
+            total += qtt * qtt_acc[bag_in]
+        qtt_acc[bag] = total
+        if bag == "shiny gold":
+            return qtt_acc[bag] - 1
---
+++ tests/day07_test.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of Advent of Code 2020
+# https://github.com/scorphus/advent-of-code-2020
+
+# Licensed under the BSD-3-Clause license:
+# https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2020, Pablo S. Blum de Aguiar <scorphus@gmail.com>
+
+from aoc.day07 import part1
+from aoc.day07 import part2
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "lines, output",
+    [
+        (
+            """\
+light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.""".rstrip().splitlines(),
+            4,
+        )
+    ],
+)
+def test_part1(lines, output):
+    assert part1(lines) == output
+
+
+@pytest.mark.parametrize(
+    "lines, output",
+    [
+        (
+            """\
+light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.""".rstrip().splitlines(),
+            32,
+        ),
+        (
+            """\
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.""".rstrip().splitlines(),
+            126,
+        ),
+    ],
+)
+def test_part2(lines, output):
+    assert part2(lines) == output
